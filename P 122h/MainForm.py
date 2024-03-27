ff2c9cdef8ba712441d4e803bf7d6a99b923b808
MasonJWelch <welch.m3@stu.janesville.k12.wi.us>
2024-03-27 10:47:15 -0500

    3/27/2024

--- P 122h/MainForm.py
+++ P 122h/MainForm.py
@@ -10,72 +10,72 @@ class MainForm(Form):
 		self.InitializeComponent()
 	
 	def InitializeComponent(self):
-		self._listBox1 = System.Windows.Forms.ListBox()
 		self._button1 = System.Windows.Forms.Button()
 		self._button2 = System.Windows.Forms.Button()
 		self._button3 = System.Windows.Forms.Button()
+		self._listBox1 = System.Windows.Forms.ListBox()
 		self.SuspendLayout()
-		# 
-		# listBox1
-		# 
-		self._listBox1.FormattingEnabled = True
-		self._listBox1.Location = System.Drawing.Point(12, 12)
-		self._listBox1.Name = "listBox1"
-		self._listBox1.Size = System.Drawing.Size(789, 355)
-		self._listBox1.TabIndex = 0
 		# 
 		# button1
 		# 
-		self._button1.Location = System.Drawing.Point(12, 373)
+		self._button1.Location = System.Drawing.Point(13, 303)
 		self._button1.Name = "button1"
 		self._button1.Size = System.Drawing.Size(75, 23)
-		self._button1.TabIndex = 1
+		self._button1.TabIndex = 0
 		self._button1.Text = "Calculate"
 		self._button1.UseVisualStyleBackColor = True
 		self._button1.Click += self.Button1Click
 		# 
 		# button2
 		# 
-		self._button2.Location = System.Drawing.Point(372, 373)
+		self._button2.Location = System.Drawing.Point(210, 303)
 		self._button2.Name = "button2"
 		self._button2.Size = System.Drawing.Size(75, 23)
-		self._button2.TabIndex = 2
+		self._button2.TabIndex = 1
 		self._button2.Text = "Clear"
 		self._button2.UseVisualStyleBackColor = True
 		self._button2.Click += self.Button2Click
 		# 
 		# button3
 		# 
-		self._button3.Location = System.Drawing.Point(726, 373)
+		self._button3.Location = System.Drawing.Point(396, 303)
 		self._button3.Name = "button3"
 		self._button3.Size = System.Drawing.Size(75, 23)
-		self._button3.TabIndex = 3
+		self._button3.TabIndex = 2
 		self._button3.Text = "Exit"
 		self._button3.UseVisualStyleBackColor = True
 		self._button3.Click += self.Button3Click
 		# 
+		# listBox1
+		# 
+		self._listBox1.FormattingEnabled = True
+		self._listBox1.Location = System.Drawing.Point(13, 12)
+		self._listBox1.Name = "listBox1"
+		self._listBox1.Size = System.Drawing.Size(458, 277)
+		self._listBox1.TabIndex = 3
+		# 
 		# MainForm
 		# 
-		self.BackColor = System.Drawing.Color.FromArgb(255, 128, 0)
-		self.ClientSize = System.Drawing.Size(813, 463)
+		self.BackColor = System.Drawing.SystemColors.ActiveCaption
+		self.ClientSize = System.Drawing.Size(483, 350)
+		self.Controls.Add(self._listBox1)
 		self.Controls.Add(self._button3)
 		self.Controls.Add(self._button2)
 		self.Controls.Add(self._button1)
-		self.Controls.Add(self._listBox1)
 		self.Name = "MainForm"
-		self.Text = "P 122a"
+		self.Text = "P 122h"
 		self.ResumeLayout(False)
 
 
 	def Button1Click(self, sender, e):
-		heading = "Number\t\tSquare\t\tSquare Root"
+		heading = "Number\tSquare\tSquare Root\tCube\t4th Root"
 		self._listBox1.Items.Add(heading)
 		for num in range(1, 20+1):
 			nsqrd = num**2
 			nsqrt = math.sqrt(num)
 			ncqrd = num**3
-			ncqrt = num**1/4
-			msg = str(num) + "\t\t" + str(nsqrd) + "\t\t" + str(round(nsqrt, 4))
+			ncqrt = num**0.25
+			msg = str(num) + "\t" + str(nsqrd) + "\t" + str(round(nsqrt, 4)) + "\t\t" + str(ncqrd) + "\t" + str(round(ncqrt, 4))
 			self._listBox1.Items.Add(msg)
 
 	def Button2Click(self, sender, e):
